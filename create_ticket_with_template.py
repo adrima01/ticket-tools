99ba7b24ee36c157222fa9a457b2eaee4ec53b8d
Adrian Maraj <adrian.maraj@yahoo.de>
2024-03-25 16:25:33 +0100

    Changed capture_exists

--- create_ticket_with_template.py
+++ create_ticket_with_template.py
@@ -125,8 +125,8 @@ def capture_exists(my_pylookyloo, id):
     output = str(tracker.get_ticket(id))
     #output = base64.b64decode(tracker.get_attachment(id)["Content"]).decode()
     #output = subprocess.check_output([rt_bin, "show", tn]).decode("utf-8")
-    capture_url = re.findall(r'h[txX][txX]ps?://[^ :"]+lookyloo.circl.lu[^ :"]+', output)
-    uuid = capture_url
+    capture_url = re.findall(r'h[txX][txX]ps?://lookyloo.circl.lu[^ :"]+', output)
+    uuid = capture_url[0].split('tree/',1)[1]
     if my_pylookyloo.get_status(uuid)['status_code'] == -1:
         uuid = -1
     return uuid
